49882444d8c914e605cde9bab751db9edcd02400
SantiG70 <davidgaviria323880@correo.itm.edu.co>
2025-09-14 12:46:31 -0500

    create controller and entites

---
+++ EmployeesWorkshop/Employees.Backend/Controllers/EmployeesController.cs
@@ -0,0 +1,32 @@
+﻿using Employees.Backend.Data;
+using Employees.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employees.Backend.Controllers;
+
+[ApiController]
+[Route("api/[Controller]")]
+public class EmployeesController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public EmployeesController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAsync()
+    {
+        return Ok(await _context.Employees.ToListAsync());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync(Employee employee)
+    {
+        _context.Employees.Add(employee);
+        await _context.SaveChangesAsync();
+        return Ok(employee);
+    }
+}
