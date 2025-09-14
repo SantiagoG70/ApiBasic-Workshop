0e2fc4551eec00d98ea63729ecb2cc57f7c4c36c
SantiG70 <davidgaviria323880@correo.itm.edu.co>
2025-09-14 09:45:53 -0500

    add class and its attributes

---
+++ EmployeesWorkshop/Employees.Shared/Entities/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Employees.Shared.Entities;
+
+public class Employee
+{
+    public int Id { get; set; }
+
+    [Display(Name = "Nombre")]
+    [Required(ErrorMessage = "El campo {0} debe de ser obligatorio")]
+    [MaxLength(30, ErrorMessage = "Maximo 30 Caracteres")]
+    public string FirstName { get; set; } = null!;
+
+    [Display(Name = "Apellido")]
+    [Required(ErrorMessage = "El campo {0} debe de ser obligatorio")]
+    [MaxLength(30, ErrorMessage = "Maximo 30 Caracteres")]
+    public string? LastName { get; set; }
+
+    [Display(Name = "Esta activo")]
+    public bool IsActive { get; set; }
+
+    [Display(Name = "Fecha de contratacion")]
+    public DateTime HireDate { get; set; }
+
+    [Display(Name = "Salario")]
+    [Required(ErrorMessage = "El salario es obligatorio")]
+    [Range(1000000, double.MaxValue, ErrorMessage = "El salario debe ser mínimo de $1,000,000")]
+    public decimal Salary { get; set; }
+}
