3f4589cea2f9cf8e4f973b5c1cb396d93c1b3b58
SantiG70 <davidgaviria323880@correo.itm.edu.co>
2025-10-09 21:06:15 -0500

    Frontend repair due to ghost file

---
+++ EmployeesWorkshop/Employees.Frontend/Program.cs
@@ -0,0 +1,25 @@
+using Employees.Frontend.Components;
+using Employees.Frontend.Repositories;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMudServices();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri("https://localhost:7041") });
+builder.Services.AddScoped<IRepository, Repository>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseAntiforgery();
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+app.Run();
