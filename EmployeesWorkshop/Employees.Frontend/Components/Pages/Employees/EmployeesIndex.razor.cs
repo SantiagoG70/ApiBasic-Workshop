5d03c0d2e62d51a9063ec8f43d441a6d6be9104c
SantiG70 <davidgaviria323880@correo.itm.edu.co>
2025-10-09 21:28:08 -0500

    Frontend with employee backend loading and load stuff

--- EmployeesWorkshop/Employees.Frontend/Components/Pages/Employees/EmployeesIndex.razor.cs
+++ EmployeesWorkshop/Employees.Frontend/Components/Pages/Employees/EmployeesIndex.razor.cs
@@ -1,5 +1,17 @@
+using Employees.Frontend.Repositories;
+using Microsoft.AspNetCore.Components;
+using Employees.Shared.Entities;
+
 namespace Employees.Frontend.Components.Pages.Employees;
 
 public partial class EmployeesIndex
 {
+    [Inject] private IRepository Repository { get; set; } = null!;
+    private List<Employee>? employees;
+
+    protected override async Task OnInitializedAsync()
+    {
+        var httpsResult = await Repository.GetAsync<List<Employee>>("/api/employees");
+        employees = httpsResult.Response;
+    }
 }
