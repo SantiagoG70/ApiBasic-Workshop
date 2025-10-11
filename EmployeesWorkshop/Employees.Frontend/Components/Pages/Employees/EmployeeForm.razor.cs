aa6290109dd36c8eeb1f89154225b51b8af4d94d
SantiG70 <davidgaviria323880@correo.itm.edu.co>
2025-10-11 17:53:23 -0500

    Create the employees

---
+++ EmployeesWorkshop/Employees.Frontend/Components/Pages/Employees/EmployeeForm.razor.cs
@@ -0,0 +1,19 @@
+using Employees.Shared.Entities;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Employees.Frontend.Components.Pages.Employees;
+
+public partial class EmployeeForm
+{
+    private EditContext editContext = null!;
+
+    [EditorRequired, Parameter] public Employee Employee { get; set; } = null!;
+    [EditorRequired, Parameter] public EventCallback OnValidSubmit { get; set; }
+    [EditorRequired, Parameter] public EventCallback ReturnAction { get; set; }
+
+    protected override void OnInitialized()
+    {
+        editContext = new(Employee);
+    }
+}
