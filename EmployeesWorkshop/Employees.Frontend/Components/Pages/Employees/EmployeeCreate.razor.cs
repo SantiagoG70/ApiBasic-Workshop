77eff96ae905b55512fefc5dce6333adf3345be3
SantiG70 <davidgaviria323880@correo.itm.edu.co>
2025-10-11 16:55:09 -0500

    Frontend preparation with elimination method and listing

---
+++ EmployeesWorkshop/Employees.Frontend/Components/Pages/Employees/EmployeeCreate.razor.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Employees.Frontend.Components.Pages.Countries;
+
+public partial class EmployeeCreate
+{
+}
